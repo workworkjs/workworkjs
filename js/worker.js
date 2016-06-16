9cbd73a85b4f8cbb206aff1a2574fc0e7711b7b7
John Chen <john@Johns-MacBook-Pro-2.local>
2016-06-16 19:05:47 -0400

    working primes

--- js/worker.js
+++ js/worker.js
@@ -1,12 +1,41 @@
-self.addEventListener('message', function(e) {
-  var num = e.data;
-  function fibonacciNoCache(num) {
-    if (num <= 1) return num;
-    return fibonacciNoCache(num - 1) + fibonacciNoCache(num - 2);
-  }
+function isPrime (num) {
+	var current = 2;
+	while (current < num) {
+		if (num < 2) {
+			return false;
+		}
+		if (current > Math.sqrt(num)) {
+			return true;
+		}
+		if (num % current === 0) {
+			return false;
+		}
+		current += 1;
+	}
+	return true;
+}
 
-  var result = fibonacciNoCache(num);
-  console.log(result);
-  self.postMessage(result);
-  self.close();
+function nthPrime (n) {
+	var primes = [2,3,5];
+	var current = 5;
+
+	while (primes.length < n) {
+		current += 2;
+		if (isPrime(current)) primes.push(current);
+
+		if (primes.length === n) break;
+		current += 4;
+		if (isPrime(current)) primes.push(current);
+	}
+
+	return primes[n - 1];
+}
+
+
+self.addEventListener('message', function(e) {
+	var num = e.data;
+	var result = nthPrime(num);
+	console.log(result);
+	self.postMessage(result);
+	self.close();
 }, false);
